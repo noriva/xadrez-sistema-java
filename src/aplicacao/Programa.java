76899c63e250afb969dc4787bfc4d8d224aab17a
noriva <jrmarson.nmj@gmail.com>
2020-05-26 12:06:34 -0300

    Primeira classe : Position 26-05-2020

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,10 +1,14 @@
 package aplicacao;
 
+import boardgame.Position;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello hord");
+		
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 		
 
 	}
