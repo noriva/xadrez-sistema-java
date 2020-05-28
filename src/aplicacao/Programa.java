85c0c0e40910bbd925d085074d2e527553321a21
noriva <jrmarson.nmj@gmail.com>
2020-05-28 11:41:07 -0300

    starting to implements Board and Piece 28-05-2020

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,14 +1,13 @@
 package aplicacao;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		
 		
 
 	}
