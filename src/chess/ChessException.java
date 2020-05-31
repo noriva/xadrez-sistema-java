e2f5caeda43ac34523f0041616d786c0ad0c9b8a
noriva <jrmarson.nmj@gmail.com>
2020-05-31 12:28:01 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,17 @@
+package chess;
+
+public class ChessException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public ChessException(String msg) {
+		// TODO Auto-generated constructor stub
+		
+		super(msg);
+	}
+	
+
+}
