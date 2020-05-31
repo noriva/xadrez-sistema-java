f2806be7b3bff3bab61335e217aeb4771db44f2c
noriva <jrmarson.nmj@gmail.com>
2020-05-31 14:58:38 -0300

    Moving Pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -37,7 +37,10 @@ public class Board {
 		return pieces[row][column];
 
 	}
-
+	
+	
+	
+	
 	public Piece piece(Position position) {
 
 		if (!positionExists(position)) {
@@ -58,7 +61,29 @@ public class Board {
 		piece.position = position;
 
 	}
-
+	
+	public Piece removePiece(Position position) {
+		
+		if(!positionExists(position)) {
+			
+			throw new BoardException("Position not on the board");
+			
+		}
+		
+		if(piece(position) == null) {
+			return null ;
+		}
+		
+		
+		Piece aux = piece(position);
+		aux.position = null ;
+		pieces[position.getRow()][position.getColumn()] = null ;
+		return aux ;
+		
+		
+		
+	}
+	
 	private boolean positionExists(int row, int column) {
 
 		return row >= 0 && row < rows && column >= 0 && column < columns;
