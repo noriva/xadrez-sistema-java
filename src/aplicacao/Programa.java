38232678bf9f6f9c2a18aff0019006066fbdeaff
noriva <jrmarson.nmj@gmail.com>
2020-06-03 10:09:10 -0300

    Handling captured pieces

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,6 +1,8 @@
 package aplicacao;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -15,13 +17,14 @@ public class Programa {
 
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
 
 		while (true) {
 
 			try {
 
 				UI.clearScreen();
-				UI.printMatch(chessMatch);
+				UI.printMatch(chessMatch , captured);
 
 				System.out.println();
 				System.out.print("Source : ");
@@ -36,6 +39,11 @@ public class Programa {
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+				
+				if(capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
+				
 
 			} catch (ChessException e) {
 				// TODO: handle exception
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,5 +1,8 @@
 package chess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
@@ -12,6 +15,9 @@ public class ChessMatch {
 	private int turn ;
 	private Color currentPlayer ;
 	private Board board;
+	
+	private List<Piece> piecesOnTheBoard = new ArrayList<>();
+	private List<Piece> capturePieces = new ArrayList<>();
 
 	public ChessMatch() {
 		// TODO Auto-generated constructor stub
@@ -69,6 +75,14 @@ public class ChessMatch {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
+		
+		if(capturedPiece != null) {
+			
+			piecesOnTheBoard.remove(capturedPiece);
+			capturePieces.add(capturedPiece);
+			
+		}
+		
 		return capturedPiece;
 		
 	}
@@ -103,6 +117,9 @@ public class ChessMatch {
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+		
+		piecesOnTheBoard.add(piece);
+		
 
 	}
 
